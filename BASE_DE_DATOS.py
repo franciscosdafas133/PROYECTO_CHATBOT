77eb801b0d9f03cbb2766c15f204492ca41f8aa0
KENNETH <kenneth.garcializarbe@gmail.com>
2024-12-10 18:43:48 -0500

    primera modificacion

---
+++ BASE_DE_DATOS.py
@@ -0,0 +1,6 @@
+import pytesseract
+
+# Configurar la ruta de Tesseract
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+print("Pytesseract configurado correctamente.")
